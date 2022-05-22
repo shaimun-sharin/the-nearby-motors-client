8d6e984a9e2742e09cb842f1ceb714828c86e29b
shaimun-sharin <shaimunsharin@gmail.com>
2022-05-22 21:05:41 +0600

    react router and daisy UI related work

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import Home from "./Pages/Home/Home";
+import Login from "./Pages/Login/Login";
+import Header from "./Pages/Shared/Header";
 
 function App() {
   return (
     <div>
-      <button class="btn btn-primary">Button</button>
+      <Header></Header>
+      <Routes>
+        <Route path="/" element={<Home></Home>}></Route>
+        <Route path="login" element={<Login></Login>}></Route>
+      </Routes>
     </div>
   );
 }
