af57186a969cfdc3b60149a54cf24512cd47bce5
shaimun-sharin <shaimunsharin@gmail.com>
2022-05-23 20:16:45 +0600

    fake image and description added

--- src/Pages/Home/Products/Product.js
+++ src/Pages/Home/Products/Product.js
@@ -4,16 +4,15 @@ const Product = ({ item }) => {
   const { name, img, price, about } = item;
   return (
     <div>
-      <div class="card w-96">
-        <figure>
-          <img src={img} alt="car!" />
+      <div class="card w-96 bg-base-100 shadow-xl">
+        <figure class="px-10 pt-10">
+          <img src={img} alt="Shoes" class="rounded-xl" />
         </figure>
-        <div class="card-body">
+        <div class="card-body items-center text-center">
           <h2 class="card-title">{name}</h2>
           <p>{about}</p>
-          <p>{price}</p>
-          <div class="card-actions justify-end">
-            <button class="btn btn-primary">Learn now!</button>
+          <div class="card-actions">
+            <button class="btn btn-primary">Buy Now</button>
           </div>
         </div>
       </div>
