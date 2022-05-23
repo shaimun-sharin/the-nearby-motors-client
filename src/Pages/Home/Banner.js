19720b73046e9ea79de3dca48e638129ef10d50b
shaimun-sharin <shaimunsharin@gmail.com>
2022-05-23 18:08:31 +0600

    banner created

--- src/Pages/Home/Banner.js
+++ src/Pages/Home/Banner.js
@@ -3,26 +3,20 @@ import React from "react";
 const Banner = () => {
   return (
     <div>
-      <div class="hero min-h-screen bg-secondary">
+      <div class="hero min-h-screen bg-gray-300">
         <div class="hero-content flex-col lg:flex-row">
           <img
-            src="https://i.ibb.co/QCymPBk/motor1.jpg"
+            src="https://i.ibb.co/NL1j7jf/motors2.webp"
             alt=""
             class="max-w-sm rounded-lg shadow-2xl"
           />
           <div>
-            <h1 class="text-3xl font-bold">
-              Welcome to
-              <span class="text-3xl text-blue-700 font-bold">
-                The Nearby Motors
-              </span>
-            </h1>
-            <p class="py-6">
-              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-              excepturi exercitationem quasi. In deleniti eaque aut repudiandae
-              et a id nisi.
+            <h1 class="text-5xl font-bold text-blue-800">The Nearby Motors</h1>
+            <p class="font-bold text-3xl py-6">
+              We welcome our customers with wide range of products in reasonable
+              prices. <br />
+              Because your motorbike is important to us.
             </p>
-            <button class="btn btn-primary">Get Started</button>
           </div>
         </div>
       </div>
