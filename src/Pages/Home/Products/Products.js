d5bfdcc476eba4c47fb1c1b3e22fc3eeb60c599f
shaimun-sharin <shaimunsharin@gmail.com>
2022-05-24 15:12:31 +0600

    data loaded from Database

--- src/Pages/Home/Products/Products.js
+++ src/Pages/Home/Products/Products.js
@@ -4,7 +4,7 @@ import Product from "./Product";
 const Products = () => {
   const [products, setProducts] = useState([]);
   useEffect(() => {
-    fetch("products.json")
+    fetch("http://localhost:5000/product")
       .then((res) => res.json())
       .then((data) => setProducts(data));
   }, []);
