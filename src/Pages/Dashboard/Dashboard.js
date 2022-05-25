ce99ce0aeac76f434e6853e067661ac2e7d2c0a9
shaimun-sharin <shaimunsharin@gmail.com>
2022-05-25 21:18:48 +0600

    user specifiq route created

--- src/Pages/Dashboard/Dashboard.js
+++ src/Pages/Dashboard/Dashboard.js
@@ -19,12 +19,16 @@ const Dashboard = () => {
       <div class="drawer-side">
         <label for="my-drawer-2" class="drawer-overlay"></label>
         <ul class="menu p-4 overflow-y-auto w-32 bg-base-100 text-base-content">
-          <li>
-            <Link to="/dashboard">My Orders</Link>
-          </li>
-          <li>
-            <Link to="/dashboard/review">Add A Review</Link>
-          </li>
+          {!admin && (
+            <>
+              <li>
+                <Link to="/dashboard">My Orders</Link>
+              </li>
+              <li>
+                <Link to="/dashboard/review">Add A Review</Link>
+              </li>
+            </>
+          )}
           <li>
             <Link to="/dashboard/profile">My Profile</Link>
           </li>
